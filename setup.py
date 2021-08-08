d3eaf301afa2e7f69ac42ea2feecd3317a7e7cb8
danderson <derrick@alto9.com>
2021-08-07 23:08:23 -0400

    fix pypi build

--- setup.py
+++ setup.py
@@ -5,6 +5,12 @@ from setuptools import find_packages, setup
 
 PWD = os.path.abspath(os.path.dirname(__file__))
 
+def load_readme():
+    with io.open(os.path.join(PWD, "README.rst"), "rt", encoding="utf8") as f:
+        readme = f.read()
+    # Replace img src for publication on pypi
+    return readme.replace("./docs/img/", "https://github.com/alto9/peddler/raw/master/docs/img/")
+
 def load_about():
     about = {}
     with io.open(
@@ -32,5 +38,10 @@ setup(
     include_package_data=True,
     license="AGPLv3",
     install_requires=load_requirements(),
-    entry_points={"console_scripts": ["peddler=peddler.commands.cli:main"]}
+    entry_points={"console_scripts": ["peddler=peddler.commands.cli:main"]},
+    author="alto9.com",
+    author_email="contact@alto9.com",
+    description="The Docker-based OpenCart distribution for control-freaks",
+    long_description=load_readme(),
+    long_description_content_type="text/x-rst",
 )
